ef01815609dd0aa1b3a8ae7bbdf8b2f60e0f893a
Gabriel <gabriel.f.takaki@gmail.com>
2025-12-16 11:35:38 -0300

    saving

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
